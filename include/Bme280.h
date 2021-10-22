4e143a2209fb00d58296b82973d3bc1fb7be54f9
Eduard Malokhvii <malokhvii.ee@gmail.com>
2021-10-23 00:10:11 +0300

    feat(api): add implementation for I2C

---
+++ include/Bme280.h
@@ -0,0 +1,318 @@
+#ifndef ARDUINO_BME280_INCLUDE_BME280_H_
+#define ARDUINO_BME280_INCLUDE_BME280_H_
+
+#include <math.h>
+#include <stdint.h>
+
+#ifndef ARDUINO_BME280_UNIT_TEST
+/* Arduino */
+#include <Wire.h>
+#else
+/* Mocks */
+#include <mocks/Wire.h>
+#endif
+
+enum class Bme280Mode : uint8_t { Sleep = 0b00, Forced = 0b01, Normal = 0b11 };
+
+enum class Bme280Oversampling : uint8_t {
+  Off = 0b000,
+  X1 = 0b001,
+  X2 = 0b010,
+  X4 = 0b011,
+  X8 = 0b100,
+  X16 = 0b101
+};
+
+enum class Bme280Filter : uint8_t {
+  Off = 0b000,
+  X2 = 0b001,
+  X4 = 0b010,
+  X8 = 0b011,
+  X16 = 0b100
+};
+
+enum class Bme280StandbyTime : uint8_t {
+  Ms0_5 = 0b000,
+  Ms10 = 0b110,
+  Ms20 = 0b111,
+  Ms62_5 = 0b001,
+  Ms125 = 0b010,
+  Ms250 = 0b011,
+  Ms500 = 0b100,
+  Ms1000 = 0b101
+};
+
+struct Bme280CalibrationData {
+  uint16_t digT1;  // Temperature compensation value
+  int16_t digT2;   // Temperature compensation value
+  int16_t digT3;   // Temperature compensation value
+  uint16_t digP1;  // Pressure compensation value
+  int16_t digP2;   // Pressure compensation value
+  int16_t digP3;   // Pressure compensation value
+  int16_t digP4;   // Pressure compensation value
+  int16_t digP5;   // Pressure compensation value
+  int16_t digP6;   // Pressure compensation value
+  int16_t digP7;   // Pressure compensation value
+  int16_t digP8;   // Pressure compensation value
+  int16_t digP9;   // Pressure compensation value
+  uint8_t digH1;   // Humidity compensation value
+  int16_t digH2;   // Humidity compensation value
+  uint8_t digH3;   // Humidity compensation value
+  int16_t digH4;   // Humidity compensation value
+  int16_t digH5;   // Humidity compensation value
+  int8_t digH6;    // Humidity compensation value
+
+  bool operator==(const Bme280CalibrationData &other) const;
+};
+
+struct Bme280Settings {
+  static Bme280Settings defaults() {
+    return {.mode = Bme280Mode::Normal,
+            .temperatureOversampling = Bme280Oversampling::X16,
+            .pressureOversampling = Bme280Oversampling::X16,
+            .humidityOversampling = Bme280Oversampling::X16,
+            .filter = Bme280Filter::Off,
+            .standbyTime = Bme280StandbyTime::Ms0_5};
+  }
+
+  static Bme280Settings weatherMonitoring() {
+    return {.mode = Bme280Mode::Forced,
+            .temperatureOversampling = Bme280Oversampling::X1,
+            .pressureOversampling = Bme280Oversampling::X1,
+            .humidityOversampling = Bme280Oversampling::X1,
+            .filter = Bme280Filter::Off,
+            .standbyTime = Bme280StandbyTime::Ms10};
+  }
+
+  static Bme280Settings humiditySensing() {
+    return {.mode = Bme280Mode::Forced,
+            .temperatureOversampling = Bme280Oversampling::X1,
+            .pressureOversampling = Bme280Oversampling::Off,
+            .humidityOversampling = Bme280Oversampling::X1,
+            .filter = Bme280Filter::Off,
+            .standbyTime = Bme280StandbyTime::Ms10};
+  }
+
+  static Bme280Settings indoor() {
+    return {.mode = Bme280Mode::Normal,
+            .temperatureOversampling = Bme280Oversampling::X2,
+            .pressureOversampling = Bme280Oversampling::X16,
+            .humidityOversampling = Bme280Oversampling::X1,
+            .filter = Bme280Filter::X16,
+            .standbyTime = Bme280StandbyTime::Ms1000};
+  }
+
+  static Bme280Settings gaming() {
+    return {.mode = Bme280Mode::Normal,
+            .temperatureOversampling = Bme280Oversampling::X4,
+            .pressureOversampling = Bme280Oversampling::X4,
+            .humidityOversampling = Bme280Oversampling::Off,
+            .filter = Bme280Filter::X16,
+            .standbyTime = Bme280StandbyTime::Ms0_5};
+  }
+
+  Bme280Mode mode;
+  Bme280Oversampling temperatureOversampling;
+  Bme280Oversampling pressureOversampling;
+  Bme280Oversampling humidityOversampling;
+  Bme280Filter filter;
+  Bme280StandbyTime standbyTime;
+
+  bool operator==(const Bme280Settings &other) const;
+};
+
+class Bme280 {
+ public:
+  virtual ~Bme280();
+
+  virtual uint8_t getChipId() const = 0;
+  virtual Bme280CalibrationData getCalibrationData() const = 0;
+  virtual Bme280Settings getSettings() const = 0;
+
+  virtual float getTemperature() const = 0;
+  virtual float getPressure() const = 0;
+  virtual float getHumidity() const = 0;
+
+  virtual void setSettings(const Bme280Settings settings) = 0;
+  virtual void reset() = 0;
+
+  virtual void sleep() = 0;
+  virtual void wakeUp() = 0;
+  virtual void wakeUpForced() = 0;
+};
+
+namespace internal {
+
+enum Bme280RegisterAddress : uint8_t {
+  Bme280RegisterAddressDigT1 = 0x88,
+  Bme280RegisterAddressDigT2 = 0x8A,
+  Bme280RegisterAddressDigT3 = 0x8C,
+  Bme280RegisterAddressDigP1 = 0x8E,
+  Bme280RegisterAddressDigP2 = 0x90,
+  Bme280RegisterAddressDigP3 = 0x92,
+  Bme280RegisterAddressDigP4 = 0x94,
+  Bme280RegisterAddressDigP5 = 0x96,
+  Bme280RegisterAddressDigP6 = 0x98,
+  Bme280RegisterAddressDigP7 = 0x9A,
+  Bme280RegisterAddressDigP8 = 0x9C,
+  Bme280RegisterAddressDigP9 = 0x9E,
+  Bme280RegisterAddressDigH1 = 0xA1,
+  Bme280RegisterAddressDigH2 = 0xE1,
+  Bme280RegisterAddressDigH3 = 0xE3,
+  Bme280RegisterAddressDigH4 = 0xE4,
+  Bme280RegisterAddressDigH5 = 0xE5,
+  Bme280RegisterAddressDigH6 = 0xE7,
+  Bme280RegisterAddressChipId = 0xD0,
+  Bme280RegisterAddressVersion = 0xD1,
+  Bme280RegisterAddressReset = 0xE0,
+  Bme280RegisterAddressCalibData = 0xE1,
+  Bme280RegisterAddressCtrlHum = 0xF2,
+  Bme280RegisterAddressStatus = 0xF3,
+  Bme280RegisterAddressCtrlMeas = 0xF4,
+  Bme280RegisterAddressConfig = 0xF5,
+  Bme280RegisterAddressPressureData = 0xF7,
+  Bme280RegisterAddressTemperatureData = 0xFA,
+  Bme280RegisterAddressHumidityData = 0xFD
+};
+
+union Bme280RegisterStatus {
+  struct {
+    uint8_t imUpdate : 1;
+    uint8_t none1 : 2;
+    uint8_t measuring : 1;
+    uint8_t none2 : 4;
+  };
+  uint8_t value;
+};
+
+union Bme280RegisterConfig {
+  struct {
+    uint8_t spi3wEn : 1;
+    uint8_t mone : 1;
+    uint8_t filter : 3;
+    uint8_t tSb : 3;
+  };
+  uint8_t value;
+};
+
+union Bme280RegisterCtrlHum {
+  struct {
+    uint8_t osrsH : 3;
+    uint8_t none : 5;
+  };
+  uint8_t value;
+};
+
+union Bme280RegisterCtrlMeas {
+  struct {
+    uint8_t mode : 2;
+    uint8_t osrsP : 3;
+    uint8_t osrsT : 3;
+  };
+  uint8_t value;
+};
+
+class AbstractBme280 : public Bme280 {
+ public:
+  virtual ~AbstractBme280();
+
+  uint8_t getChipId() const final;
+  Bme280CalibrationData getCalibrationData() const final;
+  Bme280Settings getSettings() const final;
+
+  float getTemperature() const final;
+  float getPressure() const final;
+  float getHumidity() const final;
+
+#ifndef ARDUINO_BME280_UNIT_TEST
+  void setSettings(const Bme280Settings settings) final;
+  void reset() final;
+#else
+  // Allows to check calls in setup()
+  void setSettings(const Bme280Settings settings);
+  void reset();
+#endif
+
+  void sleep() final;
+  void wakeUp() final;
+  void wakeUpForced() final;
+
+#ifndef ARDUINO_BME280_UNIT_TEST
+ protected:
+#endif
+
+#ifndef ARDUINO_BME280_UNIT_TEST
+  void setup();
+#else
+  // Allows to check that all implementations calls this method in begin()
+  virtual void setup();
+#endif
+
+#ifndef ARDUINO_BME280_UNIT_TEST
+ private:
+#endif
+
+  bool isMeasuring() const;
+
+#ifndef ARDUINO_BME280_UNIT_TEST
+  bool isImUpdate() const;
+  void readCalibrationData() const;
+#else
+  // Allows to check calls in setup()
+  virtual bool isImUpdate() const;
+  virtual void readCalibrationData() const;
+#endif
+
+  virtual void write8(const uint8_t registerAddress, uint8_t value) = 0;
+  virtual uint8_t read8(const uint8_t registerAddress) const = 0;
+  virtual uint16_t read16(const uint8_t registerAddress) const = 0;
+  int16_t read16Signed(const uint8_t registerAddress) const;
+  uint16_t read16LittleEndian(const uint8_t registerAddress) const;
+  int16_t read16SignedLittleEndian(const uint8_t registerAddress) const;
+  virtual uint32_t read24(const uint8_t registerAddress) const = 0;
+
+  uint8_t chipId_;
+
+  mutable Bme280CalibrationData calibrationData_;
+  mutable int32_t temperatureFine_;
+
+  mutable Bme280RegisterConfig config_;
+  mutable Bme280RegisterCtrlHum ctrlHum_;
+  mutable Bme280RegisterCtrlMeas ctrlMeas_;
+};
+
+};  // namespace internal
+
+enum class Bme280TwoWireAddress : uint8_t { Primary = 0x76, Secondary = 0x77 };
+
+#ifndef ARDUINO_BME280_UNIT_TEST
+class Bme280TwoWire final : public ::internal::AbstractBme280 {
+#else
+// Alows mocking this class
+class Bme280TwoWire : public ::internal::AbstractBme280 {
+#endif
+ public:
+  virtual ~Bme280TwoWire();
+
+  void begin();
+  void begin(const Bme280TwoWireAddress address);
+  void begin(const Bme280TwoWireAddress address, TwoWire *wire);
+
+  uint8_t getAddress() const;
+
+#ifndef ARDUINO_BME280_UNIT_TEST
+ private:
+#endif
+
+  void write8(const uint8_t registerAddress, uint8_t value);
+  uint8_t read8(const uint8_t registerAddress) const;
+  uint16_t read16(const uint8_t registerAddress) const;
+  uint32_t read24(const uint8_t registerAddress) const;
+
+  uint8_t address_;
+  TwoWire *wire_;
+};
+
+// TODO: class Bme280FourWire {};
+
+#endif  // ARDUINO_BME280_INCLUDE_BME280_H_
---
+++ src/Bme280.cc
@@ -0,0 +1,311 @@
+#include <Bme280.h>
+
+#ifndef ARDUINO_BME280_UNIT_TEST
+/* Arduino */
+#include <Arduino.h>
+#else
+/* Mocks */
+#include <mocks/Arduino.h>
+#endif
+
+bool Bme280CalibrationData::operator==(
+    const Bme280CalibrationData &other) const {
+  return digT1 == other.digT1 && digT2 == other.digT2 && digT3 == other.digT3 &&
+         digP1 == other.digP1 && digP2 == other.digP2 && digP3 == other.digP3 &&
+         digP4 == other.digP4 && digP5 == other.digP5 && digP6 == other.digP6 &&
+         digP7 == other.digP7 && digP8 == other.digP8 && digP9 == other.digP9 &&
+         digH1 == other.digH1 && digH2 == other.digH2 && digH3 == other.digH3 &&
+         digH4 == other.digH4 && digH5 == other.digH5 && digH6 == other.digH6;
+}
+
+bool Bme280Settings::operator==(const Bme280Settings &other) const {
+  return mode == other.mode &&
+         temperatureOversampling == other.temperatureOversampling &&
+         pressureOversampling == other.pressureOversampling &&
+         humidityOversampling == other.humidityOversampling &&
+         filter == other.filter && standbyTime == other.standbyTime;
+}
+
+Bme280::~Bme280() {}
+
+namespace internal {
+
+AbstractBme280::~AbstractBme280() {}
+
+uint8_t AbstractBme280::getChipId() const { return chipId_; }
+Bme280CalibrationData AbstractBme280::getCalibrationData() const {
+  return calibrationData_;
+}
+
+Bme280Settings AbstractBme280::getSettings() const {
+  config_.value = read8(Bme280RegisterAddressConfig);
+  ctrlHum_.value = read8(Bme280RegisterAddressCtrlHum);
+  ctrlMeas_.value = read8(Bme280RegisterAddressCtrlMeas);
+
+  return {
+      .mode = static_cast<Bme280Mode>(ctrlMeas_.mode),
+      .temperatureOversampling =
+          static_cast<Bme280Oversampling>(ctrlMeas_.osrsT),
+      .pressureOversampling = static_cast<Bme280Oversampling>(ctrlMeas_.osrsP),
+      .humidityOversampling = static_cast<Bme280Oversampling>(ctrlHum_.osrsH),
+      .filter = static_cast<Bme280Filter>(config_.filter),
+      .standbyTime = static_cast<Bme280StandbyTime>(config_.tSb)};
+}
+
+float AbstractBme280::getTemperature() const {
+  int32_t rawTemperature = read24(Bme280RegisterAddressTemperatureData);
+  if (rawTemperature == 0x800000) {
+    return NAN;
+  }
+  rawTemperature >>= 4;
+
+  int32_t var1 =
+      ((((rawTemperature >> 3) - ((int32_t)calibrationData_.digT1 << 1))) *
+       ((int32_t)calibrationData_.digT2)) >>
+      11;
+
+  int32_t var2 =
+      (((((rawTemperature >> 4) - ((int32_t)calibrationData_.digT1)) *
+         ((rawTemperature >> 4) - ((int32_t)calibrationData_.digT1))) >>
+        12) *
+       ((int32_t)calibrationData_.digT3)) >>
+      14;
+
+  temperatureFine_ = var1 + var2;
+
+  float temperature = (temperatureFine_ * 5 + 128) >> 8;
+  return temperature / 100;
+}
+
+float AbstractBme280::getPressure() const {
+  getTemperature();
+
+  int32_t rawPressure = read24(Bme280RegisterAddressPressureData);
+  if (rawPressure == 0x800000) {
+    return NAN;
+  }
+  rawPressure >>= 4;
+
+  int64_t var1 = ((int64_t)temperatureFine_) - 128000;
+  int64_t var2 = var1 * var1 * (int64_t)calibrationData_.digP6;
+  var2 = var2 + ((var1 * (int64_t)calibrationData_.digP5) << 17);
+  var2 = var2 + (((int64_t)calibrationData_.digP4) << 35);
+  var1 = ((var1 * var1 * (int64_t)calibrationData_.digP3) >> 8) +
+         ((var1 * (int64_t)calibrationData_.digP2) << 12);
+  var1 =
+      (((((int64_t)1) << 47) + var1)) * ((int64_t)calibrationData_.digP1) >> 33;
+
+  if (var1 == 0) {
+    return 0;
+  }
+
+  int64_t pressure = 1048576 - rawPressure;
+  pressure = (((pressure << 31) - var2) * 3125) / var1;
+  var1 = (((int64_t)calibrationData_.digP9) * (pressure >> 13) *
+          (pressure >> 13)) >>
+         25;
+  var2 = (((int64_t)calibrationData_.digP8) * pressure) >> 19;
+
+  pressure = ((pressure + var1 + var2) >> 8) +
+             (((int64_t)calibrationData_.digP7) << 4);
+  return (float)pressure / 256;
+}
+
+float AbstractBme280::getHumidity() const {
+  getTemperature();
+
+  int32_t rawHumidity = read16(Bme280RegisterAddressHumidityData);
+  if (rawHumidity == 0x8000) {
+    return NAN;
+  }
+
+  int32_t var1 = (temperatureFine_ - ((int32_t)76800));
+  var1 = (((((rawHumidity << 14) - (((int32_t)calibrationData_.digH4) << 20) -
+             (((int32_t)calibrationData_.digH5) * var1)) +
+            ((int32_t)16384)) >>
+           15) *
+          (((((((var1 * ((int32_t)calibrationData_.digH6)) >> 10) *
+               (((var1 * ((int32_t)calibrationData_.digH3)) >> 11) +
+                ((int32_t)32768))) >>
+              10) +
+             ((int32_t)2097152)) *
+                ((int32_t)calibrationData_.digH2) +
+            8192) >>
+           14));
+
+  var1 = (var1 - (((((var1 >> 15) * (var1 >> 15)) >> 7) *
+                   ((int32_t)calibrationData_.digH1)) >>
+                  4));
+  var1 = (var1 < 0) ? 0 : var1;
+  var1 = (var1 > 419430400) ? 419430400 : var1;
+
+  float humidity = (var1 >> 12);
+  return humidity / 1024;
+}
+
+void AbstractBme280::setSettings(const Bme280Settings settings) {
+  ctrlMeas_.mode = static_cast<uint8_t>(settings.mode);
+  ctrlMeas_.osrsT = static_cast<uint8_t>(settings.temperatureOversampling);
+  ctrlMeas_.osrsP = static_cast<uint8_t>(settings.pressureOversampling);
+
+  ctrlHum_.osrsH = static_cast<uint8_t>(settings.humidityOversampling);
+
+  config_.filter = static_cast<uint8_t>(settings.filter);
+  config_.tSb = static_cast<uint8_t>(settings.standbyTime);
+
+  write8(Bme280RegisterAddressCtrlMeas,
+         static_cast<uint8_t>(Bme280Mode::Sleep));
+
+  write8(Bme280RegisterAddressConfig, config_.value);
+  write8(Bme280RegisterAddressCtrlHum, ctrlHum_.value);
+  write8(Bme280RegisterAddressCtrlMeas, ctrlMeas_.value);
+
+  delay(100);
+}
+
+void AbstractBme280::reset() { write8(Bme280RegisterAddressReset, 0xB6); }
+
+void AbstractBme280::sleep() {
+  auto settings = getSettings();
+  settings.mode = Bme280Mode::Sleep;
+  setSettings(settings);
+}
+
+void AbstractBme280::wakeUp() {
+  auto settings = getSettings();
+  settings.mode = Bme280Mode::Normal;
+  setSettings(settings);
+}
+
+void AbstractBme280::wakeUpForced() {
+  auto settings = getSettings();
+  settings.mode = Bme280Mode::Forced;
+  setSettings(settings);
+}
+
+void AbstractBme280::setup() {
+  chipId_ = read8(Bme280RegisterAddressChipId);
+
+  reset();
+  delay(10);
+
+  while (isImUpdate()) {
+    delay(20);
+  }
+
+  readCalibrationData();
+  setSettings(Bme280Settings::defaults());
+}
+
+bool AbstractBme280::isImUpdate() const {
+  Bme280RegisterStatus status;
+  status.value = read8(Bme280RegisterAddressStatus);
+  return status.imUpdate != 0;
+}
+
+bool AbstractBme280::isMeasuring() const {
+  Bme280RegisterStatus status;
+  status.value = read8(Bme280RegisterAddressStatus);
+  return status.measuring == 1;
+}
+
+void AbstractBme280::readCalibrationData() const {
+  calibrationData_.digT1 = read16LittleEndian(Bme280RegisterAddressDigT1);
+  calibrationData_.digT2 = read16SignedLittleEndian(Bme280RegisterAddressDigT2);
+  calibrationData_.digT3 = read16SignedLittleEndian(Bme280RegisterAddressDigT3);
+
+  calibrationData_.digP1 = read16LittleEndian(Bme280RegisterAddressDigP1);
+  calibrationData_.digP2 = read16SignedLittleEndian(Bme280RegisterAddressDigP2);
+  calibrationData_.digP3 = read16SignedLittleEndian(Bme280RegisterAddressDigP3);
+  calibrationData_.digP4 = read16SignedLittleEndian(Bme280RegisterAddressDigP4);
+  calibrationData_.digP5 = read16SignedLittleEndian(Bme280RegisterAddressDigP5);
+  calibrationData_.digP6 = read16SignedLittleEndian(Bme280RegisterAddressDigP6);
+  calibrationData_.digP7 = read16SignedLittleEndian(Bme280RegisterAddressDigP7);
+  calibrationData_.digP8 = read16SignedLittleEndian(Bme280RegisterAddressDigP8);
+  calibrationData_.digP9 = read16SignedLittleEndian(Bme280RegisterAddressDigP9);
+
+  calibrationData_.digH1 = read8(Bme280RegisterAddressDigH1);
+  calibrationData_.digH2 = read16SignedLittleEndian(Bme280RegisterAddressDigH2);
+  calibrationData_.digH3 = read8(Bme280RegisterAddressDigH3);
+  calibrationData_.digH4 =
+      (static_cast<int8_t>(read8(Bme280RegisterAddressDigH4)) << 4) |
+      (read8(Bme280RegisterAddressDigH4 + 1) & 0xF);
+  calibrationData_.digH5 =
+      (static_cast<int8_t>(read8(Bme280RegisterAddressDigH5 + 1)) << 4) |
+      (read8(Bme280RegisterAddressDigH5) >> 4);
+  calibrationData_.digH6 =
+      static_cast<int8_t>(read8(Bme280RegisterAddressDigH6));
+}
+
+int16_t AbstractBme280::read16Signed(const uint8_t registerAddress) const {
+  return static_cast<int16_t>(read16(registerAddress));
+}
+
+uint16_t AbstractBme280::read16LittleEndian(
+    const uint8_t registerAddress) const {
+  uint16_t value = read16(registerAddress);
+  return (value >> 8) | (value << 8);
+}
+
+int16_t AbstractBme280::read16SignedLittleEndian(
+    const uint8_t registerAddress) const {
+  return static_cast<int16_t>(read16LittleEndian(registerAddress));
+}
+
+}  // namespace internal
+
+Bme280TwoWire::~Bme280TwoWire() {}
+
+void Bme280TwoWire::begin() { begin(Bme280TwoWireAddress::Primary); }
+
+void Bme280TwoWire::begin(const Bme280TwoWireAddress address) {
+  begin(address, &Wire);
+}
+
+void Bme280TwoWire::begin(const Bme280TwoWireAddress address, TwoWire *wire) {
+  address_ = static_cast<uint8_t>(address);
+  wire_ = wire;
+  setup();
+}
+
+uint8_t Bme280TwoWire::getAddress() const { return address_; }
+
+void Bme280TwoWire::write8(const uint8_t registerAddress, uint8_t value) {
+  wire_->beginTransmission(address_);
+  wire_->write(registerAddress);
+  wire_->write(value);
+  wire_->endTransmission();
+}
+
+uint8_t Bme280TwoWire::read8(const uint8_t registerAddress) const {
+  wire_->beginTransmission(address_);
+  wire_->write(registerAddress);
+  wire_->endTransmission();
+  wire_->requestFrom(address_, 1u);
+
+  return wire_->read();
+}
+
+uint16_t Bme280TwoWire::read16(const uint8_t registerAddress) const {
+  wire_->beginTransmission(address_);
+  wire_->write(registerAddress);
+  wire_->endTransmission();
+  wire_->requestFrom(address_, 2u);
+
+  return (wire_->read() << 8) | wire_->read();
+}
+
+uint32_t Bme280TwoWire::read24(const uint8_t registerAddress) const {
+  wire_->beginTransmission(address_);
+  wire_->write(registerAddress);
+  wire_->endTransmission();
+  wire_->requestFrom(address_, 3u);
+
+  uint32_t value = wire_->read();
+  value <<= 8;
+  value |= wire_->read();
+  value <<= 8;
+  value |= wire_->read();
+
+  return value;
+}
