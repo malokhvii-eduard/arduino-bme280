6f248288d2b5dece267f720e9f6c23c12c3bd505
Eduard Malokhvii <malokhvii.ee@gmail.com>
2021-10-23 00:21:53 +0300

    test(api): add some tests for Bme280TwoWire
    - Added TEST_F(Bme280TwoWireTest, BeginsWithoutAddressAndWire)
    - Added TEST_P(Bme280TwoWireTestWithAddress, BeginsWithAddressAndWithoutWire)
    - Added TEST_P(Bme280TwoWireTestWithAddress, BeginsWithAddressAndWire)
    - Added TEST_P(Bme280TwoWireTestWithAddress, GetsAddress)
    - Added TEST_P(Bme280TwoWireTestWith8BitCommunication, Writes)
    - Added TEST_P(Bme280TwoWireTestWith8BitCommunication, Reads)
    - Added TEST_P(Bme280TwoWireTestWith16BitCommunication, Reads)
    - Added TEST_P(Bme280TwoWireTestWith24BitCommunication, Reads)

---
+++ test/native/Bme280TwoWireTest.cc
@@ -0,0 +1,118 @@
+/* Google Test */
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+/* Fixtures */
+#include <fixtures/Bme280TwoWireTest.h>
+
+using ::testing::Combine;
+using ::testing::InSequence;
+using ::testing::Return;
+using ::testing::Values;
+
+TEST_F(Bme280TwoWireTest, BeginsWithoutAddressAndWire) {
+  EXPECT_CALL(sensor, setup()).Times(1);
+
+  sensor.begin();
+  EXPECT_EQ(sensor.wire_, &Wire);
+  EXPECT_EQ(sensor.address_,
+            static_cast<uint8_t>(Bme280TwoWireAddress::Primary));
+}
+
+TEST_P(Bme280TwoWireTestWithAddress, BeginsWithAddressAndWithoutWire) {
+  EXPECT_CALL(sensor, setup());
+
+  sensor.begin(GetParam());
+  EXPECT_EQ(sensor.wire_, &Wire);
+  EXPECT_EQ(sensor.address_, address);
+}
+
+TEST_P(Bme280TwoWireTestWithAddress, BeginsWithAddressAndWire) {
+  EXPECT_CALL(sensor, setup());
+
+  sensor.begin(GetParam(), &wire);
+  EXPECT_EQ(sensor.wire_, &wire);
+  EXPECT_EQ(sensor.address_, address);
+}
+
+TEST_P(Bme280TwoWireTestWithAddress, GetsAddress) {
+  sensor.address_ = address;
+  EXPECT_EQ(sensor.getAddress(), sensor.address_);
+}
+
+TEST_P(Bme280TwoWireTestWith8BitCommunication, Writes) {
+  {
+    InSequence s;
+    EXPECT_CALL(wire, beginTransmission(sensor.address_));
+    EXPECT_CALL(wire, write(registerAddress));
+    EXPECT_CALL(wire, write(expectedValue));
+    EXPECT_CALL(wire, endTransmission());
+  }
+
+  sensor.write8(registerAddress, expectedValue);
+}
+
+TEST_P(Bme280TwoWireTestWith8BitCommunication, Reads) {
+  {
+    InSequence s;
+    EXPECT_CALL(wire, beginTransmission(sensor.address_));
+    EXPECT_CALL(wire, write(registerAddress));
+    EXPECT_CALL(wire, endTransmission());
+    EXPECT_CALL(wire, requestFrom(sensor.address_, 1u));
+    EXPECT_CALL(wire, read()).WillOnce(Return(expectedValue));
+  }
+
+  EXPECT_EQ(sensor.read8(registerAddress), expectedValue);
+}
+
+TEST_P(Bme280TwoWireTestWith16BitCommunication, Reads) {
+  {
+    InSequence s;
+    EXPECT_CALL(wire, beginTransmission(sensor.address_));
+    EXPECT_CALL(wire, write(registerAddress));
+    EXPECT_CALL(wire, endTransmission());
+    EXPECT_CALL(wire, requestFrom(sensor.address_, 2u));
+    EXPECT_CALL(wire, read())
+        .WillOnce(Return(communicationSnapshot[1]))
+        .WillOnce(Return(communicationSnapshot[2]));
+  }
+
+  EXPECT_EQ(sensor.read16(registerAddress), expectedValue);
+}
+
+TEST_P(Bme280TwoWireTestWith24BitCommunication, Reads) {
+  {
+    InSequence s;
+    EXPECT_CALL(wire, beginTransmission(sensor.address_));
+    EXPECT_CALL(wire, write(registerAddress));
+    EXPECT_CALL(wire, endTransmission());
+    EXPECT_CALL(wire, requestFrom(sensor.address_, 3u));
+    EXPECT_CALL(wire, read())
+        .WillOnce(Return(communicationSnapshot[1]))
+        .WillOnce(Return(communicationSnapshot[2]))
+        .WillOnce(Return(communicationSnapshot[3]));
+  }
+
+  EXPECT_EQ(sensor.read24(registerAddress), expectedValue);
+}
+
+#define PARAM_ADDRESS \
+  Values(Bme280TwoWireAddress::Primary, Bme280TwoWireAddress::Secondary)
+
+INSTANTIATE_TEST_SUITE_P(Addresses, Bme280TwoWireTestWithAddress,
+                         PARAM_ADDRESS);
+
+INSTANTIATE_TEST_SUITE_P(P_8_BIT_MESSAGES,
+                         Bme280TwoWireTestWith8BitCommunication,
+                         Combine(PARAM_ADDRESS, PARAM_8_BIT_REGISTER,
+                                 PARAM_8_BIT_COMMUNICATION_SNAPSHOT));
+
+INSTANTIATE_TEST_SUITE_P(P_16_BIT_MESSAGES,
+                         Bme280TwoWireTestWith16BitCommunication,
+                         Combine(PARAM_ADDRESS, PARAM_16_BIT_REGISTER,
+                                 PARAM_16_BIT_COMMUNICATION_SNAPSHOT));
+
+INSTANTIATE_TEST_SUITE_P(P_24_BIT_MESSAGES,
+                         Bme280TwoWireTestWith24BitCommunication,
+                         Combine(PARAM_ADDRESS, PARAM_24_BIT_REGISTER,
+                                 PARAM_24_BIT_COMMUNICATION_SNAPSHOT));
---
+++ test/native/fixtures/Bme280TwoWireTest.h
@@ -0,0 +1,59 @@
+#ifndef ARDUINO_BME280_TEST_NATIVE_FIXTURES_BME280_TWO_WIRE_TEST_H_
+#define ARDUINO_BME280_TEST_NATIVE_FIXTURES_BME280_TWO_WIRE_TEST_H_
+
+/* Google Test */
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+/* Mocks */
+#include <mocks/Bme280.h>
+
+/* Test Parameters */
+#include <params/Communication.h>
+
+class Bme280TwoWireTest : public ::testing::Test {
+ protected:
+  TwoWireMock wire;
+  Bme280TwoWireMock sensor;
+};
+
+class Bme280TwoWireTestWithAddress
+    : public Bme280TwoWireTest,
+      public ::testing::WithParamInterface<Bme280TwoWireAddress> {
+ protected:
+  void SetUp() override { address = static_cast<uint8_t>(GetParam()); }
+
+  uint8_t address;
+};
+
+class Bme280TwoWireTestWithCommunication
+    : public ::testing::TestWithParam<
+          std::tuple<Bme280TwoWireAddress, uint8_t, CommunicationSnapshot>> {
+ protected:
+  void SetUp() override {
+    sensor.wire_ = &wire;
+    sensor.address_ = static_cast<uint8_t>(std::get<0>(GetParam()));
+
+    registerAddress = std::get<1>(GetParam());
+    communicationSnapshot = std::get<2>(GetParam());
+    expectedValue = communicationSnapshot[0];
+  }
+
+  ::testing::StrictMock<TwoWireMock> wire;
+  Bme280TwoWire sensor;
+
+  uint8_t registerAddress;
+  CommunicationSnapshot communicationSnapshot;
+  long expectedValue;
+};
+
+class Bme280TwoWireTestWith8BitCommunication
+    : public Bme280TwoWireTestWithCommunication {};
+
+class Bme280TwoWireTestWith16BitCommunication
+    : public Bme280TwoWireTestWithCommunication {};
+
+class Bme280TwoWireTestWith24BitCommunication
+    : public Bme280TwoWireTestWithCommunication {};
+
+#endif  // ARDUINO_BME280_TEST_NATIVE_FIXTURES_BME280_TWO_WIRE_TEST_H_
